b37d397c96f87d38a6799b4607f8d335ecff4c51
andrewgraves(MAC) <andrewcgraves@gmail.com>
2019-12-20 13:10:43 -0800

    Initial Commit

---
+++ DiaryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by Andrew Graves on 12/20/19.
+//  Copyright © 2019 Andrew Graves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
