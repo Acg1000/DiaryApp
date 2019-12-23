10ed48b87b9e5045acbe2298ca5ebf85fa28bd84
andrewgraves(MAC) <andrewcgraves@gmail.com>
2019-12-22 22:07:54 -0800

    Photo Saving functionality works!

--- DiaryApp/Controllers/CreateEntryController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CreateEntryController.swift
-//  DiaryApp
-//
-//  Created by Andrew Graves on 12/20/19.
-//  Copyright © 2019 Andrew Graves. All rights reserved.
-//
-
-import Foundation
--- DiaryApp/Controllers/MainPageController.swift
+++ DiaryApp/Controllers/MainPageController.swift
@@ -16,8 +16,7 @@ class MainPageController: UITableViewController {
     let dateFormatter = DateFormatter()
     let context = CoreDataStack().managedObjectContext
     
-    @IBOutlet weak var entryTextField: UITextField!
-
+    @IBOutlet weak var currentDate: UILabel!
     
     lazy var dataSource: EntriesDataSource = {
         let request: NSFetchRequest<Entry> = Entry.fetchRequest()
@@ -27,6 +26,7 @@ class MainPageController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCurrentDates()
         
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 600
@@ -34,92 +34,38 @@ class MainPageController: UITableViewController {
         
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-        
-//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-//
-//        let managedContext = appDelegate.persistentContainer.viewContext
-//
-//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Entry")
-//
-//        do {
-//            entries = try managedContext.fetch(fetchRequest)
-//
-//        } catch let error as NSError {
-//            print("could not fetch. \(error), \(error.userInfo)")
-//        }
-//    }
-
-//    // MARK: - Table view data source
-//
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return entries.count
-//    }
-//
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let entry = entries[indexPath.row]
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath) as! EntryCell
-//
-////        let date = entry.value(forKey: "date") as? Date
-////        dateFormatter.dateStyle = .full
-//        let date = entry.date
-//        cell.setDate(date: date)
-//
-////        let description = entry.value(forKey: "entryDescription") as? String
-//        let description = entry.entryDescription
-//        cell.descriptionLabel.text = description
-//
-////        if let date = date, let description = description {
-////            cell.titleLabel.text = dateFormatter.string(from: date)
-////            cell.descriptionLabel.text = description
-////
-////            return cell
-////
-////        } else {
-////            fatalError()
-////        }
-//        return cell
-//    }
-    
-    
-    @IBAction func savePressed(_ sender: Any) {
-        if let text = entryTextField.text {
-//            save(description: text)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "createEntry" {
+//            let createEntryController = segue.destination as? CreateEntryController
             
-            print("save pressed")
-            
-            let _ = Entry.with(text, status: .happy, location: nil, photo: nil, in: context)
-            context.saveChanges()
-            tableView.reloadData()
         }
     }
     
-//    func save(description: String) {
-//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+    
+    func setCurrentDates() {
+        dateFormatter.dateStyle = .full
+        currentDate.text = dateFormatter.string(from: Date())
+        navigationController?.title = dateFormatter.string(from: Date())
+    }
+    
+    @IBAction func recordEntryPressed(_ sender: Any) {
+//        guard let storyboard = storyboard else { return }
 //
-//        let managedContext = appDelegate.persistentContainer.viewContext
+//        let createEntryController = storyboard.instantiateViewController(withIdentifier: String(describing: CreateEntryController.self)) as! CreateEntryController
 //
-//        let entity = NSEntityDescription.entity(forEntityName: "Entry", in: managedContext)!
+//        let navController = UINavigationController(rootViewController: createEntryController)
+//        navigationController?.present(navController, animated: true, completion: nil)
+    }
+    
+    @IBAction func savePressed(_ sender: Any) {
+        
+//        if let text = entryTextField.text {
 //
-//        let entry = NSManagedObject(entity: entity, insertInto: managedContext)
+//            print("save pressed")
 //
-//        entry.setValue(Date(), forKey: "date")
-//        entry.setValue(description, forKey: "entryDescription")
-//
-//        do {
-//            try managedContext.save()
-//            entries.append(entry)
-//
-//        } catch let error as NSError {
-//            print("Count not save. \(error), \(error.userInfo)")
+//            let _ = Entry.with(text, status: .happy, location: nil, photo: nil, in: context)
+//            context.saveChanges()
+//            tableView.reloadData()
 //        }
-//    }
+    }
 }
