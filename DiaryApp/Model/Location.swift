c0e90aa91f8259b4672b76aaba67043e71288551
andrewgraves(MAC) <andrewcgraves@gmail.com>
2019-12-26 11:09:15 -0800

    Moved the last of the files

---
+++ DiaryApp/Model/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  DiaryApp
+//
+//  Created by Andrew Graves on 12/23/19.
+//  Copyright © 2019 Andrew Graves. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct Location {
+    let latitude: Double
+    let longitude: Double
+    let placemark: CLPlacemark
+}
